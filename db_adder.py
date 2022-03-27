6d014fa5c2d886e43bd0bf1baf9f2be8414218cf
emilk <emiliadembek98@gmail.com>
2022-03-28 01:38:38 +0200

    .

--- db_adder.py
+++ db_adder.py
@@ -2,10 +2,13 @@ import db_com
 
 if __name__ == "__main__":
     db_com.init()
-    file_handler=open("products.txt", "r", encoding="UTF-8") #otwieramy plik txt do trybu read
-    lines=file_handler.readlines()
+    file_handler = open("products.txt", "r", encoding="UTF-8")
+    lines = file_handler.readlines()
     for line in lines:
-        tmp=line.split(";")
-        tmp[2]=tmp[2].rstrip()
-        db_com.insert_product(tmp[0],tmp[1],tmp[2])
+        tmp = line.split(";")
+        tmp[2] = tmp[2].rstrip()
+        db_com.insert_product(tmp[0], tmp[1], tmp[2])
+
+
+
 
--- db_com.py
+++ db_com.py
@@ -1,33 +1,36 @@
 import os
 
-import psycopg2 #łaczy z bazą
+import psycopg2  # łaczy z bazą
 import dotenv
 
 config = {}
 
-def init(): #init database
+
+def init():  # init database
     dotenv.load_dotenv()
-    config["db_ip"]=os.environ.get("db_ip")
-    config["db_name"]=os.environ.get("db_name")
-    config["db_user"]=os.environ.get("db_user")
-    config["db_password"]=os.environ.get("db_password")
+    config["db_ip"] = os.environ.get("db_ip")
+    config["db_name"] = os.environ.get("db_name")
+    config["db_user"] = os.environ.get("db_user")
+    config["db_password"] = os.environ.get("db_password")
 
 
 def get_product(ean):
-    pg_handler=psycopg2.connect(host=config["db_ip"],database=config["db_name"], user=config["db_user"], password=config["db_password"])
-    pg_cursor=pg_handler.cursor()
-    pg_cursor.execute(f"SELECT * FROM products WHERE EAN = '{ean}';")
-    result= pg_cursor.fetchall() #daje wszystkie wyniki
-    pg_handler.close()
-    return result
-
-def insert_product(ean,name,bin): #dodawanie do bazy
     pg_handler = psycopg2.connect(host=config["db_ip"], database=config["db_name"], user=config["db_user"],
                                   password=config["db_password"])
     pg_cursor = pg_handler.cursor()
     pg_cursor.execute(f"SELECT * FROM products WHERE EAN = '{ean}';")
     result = pg_cursor.fetchall()
-    if len(result)!=0:
+    pg_handler.close()
+    return result
+
+
+def insert_product(ean, name, bin):
+    pg_handler = psycopg2.connect(host=config["db_ip"], database=config["db_name"], user=config["db_user"],
+                                  password=config["db_password"])
+    pg_cursor = pg_handler.cursor()
+    pg_cursor.execute(f"SELECT * FROM products WHERE EAN = '{ean}';")
+    result = pg_cursor.fetchall()
+    if len(result) != 0:
         print(f"{ean} already exists")
         return
 
